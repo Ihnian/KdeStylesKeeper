972d55a0faab5745609d7aba9bd6714e9b69e885
Ihnian <jakub.kaczmarekb@gmail.com>
2026-01-23 22:14:20 +0100

    Stuff

--- Main.py
+++ Main.py
@@ -7,16 +7,18 @@ from PySide6.QtWidgets import QApplication, QPushButton, QLabel,QFileDialog
 from PySide6.QtCore import Slot
 from PySide6 import QtCore, QtWidgets, QtGui
 import os
+import threading
 
 #Main class
 class App(QtWidgets.QWidget):
     #Layout
-    #github bug
     def __init__(self):
         super().__init__()
         #none direction
         self.direction = ""
+        self.value = 0
         #Ui components
+        self.progressbar = QtWidgets.QProgressBar(value=self.value, maximum=100)
         self.direction_button = QtWidgets.QPushButton("Choose directory")
         self.copy_button = QtWidgets.QPushButton("Copy")
         self.label = QtWidgets.QLabel("choose direction",
@@ -27,6 +29,7 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.direction_button)
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.copy_button)
+        self.layout.addWidget(self.progressbar)
         #button clicked
         self.direction_button.clicked.connect(self.get_direction)
         self.copy_button.clicked.connect(self.cloning)
@@ -50,38 +53,42 @@ class App(QtWidgets.QWidget):
         if (destination == ""):
             alert = QtWidgets.QLabel("Please choose folder")
             alert.show()
+        else:
+            desktoptheme = pathlib.Path("/usr/share/plasma/desktoptheme/")
+            desktoptheme_folder = os.path.join(destination, "desktop_theme")
+            os.makedirs(desktoptheme_folder, exist_ok=True)
+            shutil.copytree(desktoptheme, desktoptheme_folder, dirs_exist_ok=True)
+            self.value = 15
 
-        desktoptheme = pathlib.Path("/usr/share/plasma/desktoptheme/")
-        desktoptheme_folder = os.path.join(destination, "desktop_theme")
-        os.makedirs(desktoptheme_folder, exist_ok=True)
-        shutil.copytree(desktoptheme, desktoptheme_folder, dirs_exist_ok=True)
-        
-        look_and_feel_folder = os.path.join(destination, "look-and-feel")
-        look_and_feel = pathlib.Path(f"{home_dir}/.local/share/plasma/look-and-feel/")
-        os.makedirs(look_and_feel_folder, exist_ok=True)
-        shutil.copytree(look_and_feel, look_and_feel_folder, dirs_exist_ok=True)
+            look_and_feel_folder = os.path.join(destination, "look-and-feel")
+            look_and_feel = pathlib.Path(f"{home_dir}/.local/share/plasma/look-and-feel/")
+            os.makedirs(look_and_feel_folder, exist_ok=True)
+            shutil.copytree(look_and_feel, look_and_feel_folder, dirs_exist_ok=True)
+            self.value = 35
 
-        kdeglobals = pathlib.Path(f"{home_dir}/.config/kdeglobals")
-        appletsrc = pathlib.Path(f"{home_dir}/.config/plasma-org.kde.plasma.desktop-appletsrc")
-        plasmarc = pathlib.Path(f"{home_dir}/.config/plasmarc")
-        config_folder = os.path.join(destination, "Config")
-        os.makedirs(config_folder, exist_ok=True)
-        shutil.copy(kdeglobals, config_folder)
-        shutil.copy(plasmarc, config_folder)
-        shutil.copy(appletsrc, config_folder)
+            kdeglobals = pathlib.Path(f"{home_dir}/.config/kdeglobals")
+            appletsrc = pathlib.Path(f"{home_dir}/.config/plasma-org.kde.plasma.desktop-appletsrc")
+            plasmarc = pathlib.Path(f"{home_dir}/.config/plasmarc")
+            config_folder = os.path.join(destination, "Config")
+            os.makedirs(config_folder, exist_ok=True)
+            shutil.copy(kdeglobals, config_folder)
+            shutil.copy(plasmarc, config_folder)
+            shutil.copy(appletsrc, config_folder)
+            self.value = 70
 
-        icons  = pathlib.Path(f"{home_dir}/.local/share/icons/")
-        icons_folder = os.path.join(destination, "Icons")
-        os.makedirs(icons_folder, exist_ok=True)
-        shutil.copytree(icons, icons_folder, dirs_exist_ok=True)
-        
-        plasmoid = pathlib.Path(f"{home_dir}/.local/share/plasma/plasmoids/")
-        plasmoid_folder = os.path.join(destination, "plasmoid")
-        os.makedirs(plasmoid_folder, exist_ok=True)
-        shutil.copytree(plasmoid, plasmoid_folder, dirs_exist_ok=True)
-
-        
-        self.label.setText("Done")
+            icons  = pathlib.Path(f"{home_dir}/.local/share/icons/")
+            icons_folder = os.path.join(destination, "Icons")
+            os.makedirs(icons_folder, exist_ok=True)
+            shutil.copytree(icons, icons_folder, dirs_exist_ok=True)
+            self.value = 85
+            
+            plasmoid = pathlib.Path(f"{home_dir}/.local/share/plasma/plasmoids/")
+            plasmoid_folder = os.path.join(destination, "plasmoid")
+            os.makedirs(plasmoid_folder, exist_ok=True)
+            shutil.copytree(plasmoid, plasmoid_folder, dirs_exist_ok=True)
+            self.value = 100
+            
+            self.label.setText("Done")
 
 
 
